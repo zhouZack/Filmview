31efefd774eb1a69f8b84ce67d329b0525db785a
zc_zhou <zhoucheng35435@163.com>
2015-12-30 11:21:18 +0800

    新分支开发

---
+++ FilmView/FilmView/classes/Main/other/view/StarView.h
@@ -0,0 +1,16 @@
+//
+//  StarView.h
+//  FilmView
+//
+//  Created by Zc_zhou on 15/12/30.
+//  Copyright © 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StarView : UIView
+
+
+- (void)setStarView:(double)num;
+
+@end
